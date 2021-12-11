207863f2a918ec1ea29a10f702184ba64f20004f
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-12-11 15:46:19 +0400

    Update Action bar

--- app/src/main/java/dev/zurbaevi/quoteoftheday/presentation/main/QuoteActivity.kt
+++ app/src/main/java/dev/zurbaevi/quoteoftheday/presentation/main/QuoteActivity.kt
@@ -2,7 +2,12 @@ package dev.zurbaevi.quoteoftheday.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import dev.zurbaevi.quoteoftheday.R
 import dev.zurbaevi.quoteoftheday.databinding.ActivityQuoteBinding
 
 @AndroidEntryPoint
@@ -10,10 +15,22 @@ class QuoteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQuoteBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
