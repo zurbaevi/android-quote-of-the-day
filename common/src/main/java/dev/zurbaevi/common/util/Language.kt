ab9d664b96c7b28d350dfd66ad4ce62d4767e091
zurbaevi <zurbaevi.nika@gmail.com>
2022-04-02 03:05:38 +0400

    Update Language.kt

--- common/src/main/java/dev/zurbaevi/common/util/Language.kt
+++ common/src/main/java/dev/zurbaevi/common/util/Language.kt
@@ -1,21 +1,5 @@
 package dev.zurbaevi.common.util
 
-
-//enum class Language(val language: String) {
-//    ENGLISH("en"),
-//    RUSSIAN("ru");
-//
-//    companion object {
-//        fun create(x: String): Language {
-//            return when (x) {
-//                "en" -> ENGLISH
-//                "ru" -> RUSSIAN
-//                else -> throw IllegalStateException()
-//            }
-//        }
-//    }
-//}
-
 class Language {
     companion object {
         const val ENGLISH = "en"
