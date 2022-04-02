2104d17ac5e2fc41508c74521f988b61b190f5c4
zurbaevi <zurbaevi.nika@gmail.com>
2022-04-02 14:33:42 +0400

    Make class Language.kt to object Language.kt

--- common/src/main/java/dev/zurbaevi/common/util/Language.kt
+++ common/src/main/java/dev/zurbaevi/common/util/Language.kt
@@ -1,16 +1,14 @@
 package dev.zurbaevi.common.util
 
-class Language {
-    companion object {
-        const val ENGLISH = "en"
-        const val RUSSIAN = "ru"
+object Language {
+    const val ENGLISH = "en"
+    const val RUSSIAN = "ru"
 
-        fun create(x: String): String {
-            return when (x) {
-                "en" -> RUSSIAN
-                "ru" -> ENGLISH
-                else -> throw IllegalStateException()
-            }
+    fun create(x: String): String {
+        return when (x) {
+            "en" -> RUSSIAN
+            "ru" -> ENGLISH
+            else -> throw IllegalStateException()
         }
     }
 }
