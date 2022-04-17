3bc51e31da36362cf41c5fafefa07e1b6437b781
zurbaevi <zurbaevi.nika@gmail.com>
2022-04-17 21:15:58 +0400

    Add splash screen

--- app/src/main/java/dev/zurbaevi/quoteoftheday/presentation/MainActivity.kt
+++ app/src/main/java/dev/zurbaevi/quoteoftheday/presentation/MainActivity.kt
@@ -1,8 +1,17 @@
 package dev.zurbaevi.quoteoftheday.presentation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.zurbaevi.quoteoftheday.R
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
+
+}
